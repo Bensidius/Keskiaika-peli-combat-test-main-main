e4dc6dae9c24ab4eaf617b867cbb3f96a572e262
Bensidius <jani.havejankku@hotmail.com>
2022-02-23 14:21:18 +0200

    struggle

--- Assets/Scripts/BatGFX.cs
+++ Assets/Scripts/BatGFX.cs
@@ -6,7 +6,40 @@ using Pathfinding;
 public class BatGFX : MonoBehaviour
 {
 
+public bool isDead;
+
+private Rigidbody2D rb;
+
+public float speed = 5;
+
+
+
   public AIPath aiPath;
+
+  void Start()
+  {
+      rb = GetComponent<Rigidbody2D>();
+  }
+
+void FixedUpdate()
+    {
+// kuolon hetki
+      isDead = GetComponent<Enemy>().Isdead;
+        // if alive, keep moving
+      if (!isDead)
+      {
+          rb.velocity = new Vector2(speed, 0f);
+      } 
+     if (isDead)
+      {
+          rb.velocity = new Vector2(0f, 0f);
+      } 
+
+
+
+
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -20,4 +53,9 @@ public class BatGFX : MonoBehaviour
            transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
        }
     }
-}
+
+       
+} 
+
+
+
